edc510c6cb1922b99b76bc0b9f2a91db71869f69
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2024-12-11 17:49:24 +0300

    I'm now focusing on the mobile version of the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,25 @@
+import { useState } from "react"
+
 const Navbar = () => {
+    const [open, setOpen] = useState(false)
   return (
-    <section>
+    <section className="w-full h-16 md:h-20 flex items-center justify-between">
         {/* logo */}
-        <div className="">
-            <img src="" alt="" />
+        <div className="flex items-center gap-4 text-2xl font-bold">
+            <img src="/logo.png" className="w-8 h-8" alt="logo" />
+            <span>CodebaseBlogs</span>
         </div>
         {/* Menu mobile */}
+        <div className="md:hidden">
+            <div
+            className="cursor-pointer text-4xl"
+            onClick={()=>setOpen(prev => !prev)}
+            >
+                {open? 'X' : "☰" }
+            </div>
+        </div>
         {/* Menu Desktop */}
+        <div className="hidden md:flex">D</div>
     </section>
   )
 }
