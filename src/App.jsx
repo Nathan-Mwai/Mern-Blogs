8485461256486f7affc37cd2036e2035f0fb12c9
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2024-12-11 15:00:11 +0300

    Now doing the navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import Navbar from "./components/Navbar"
+
 const App = () => {
   return (
-    // THis is for creating a padding for the head on different device settings
-    <div className='px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64'>Hello World</div>
+    // This is for creating a padding on different device settings
+    <div className='px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64'>
+      <Navbar/>
+      {/* <BreadCrumbs/> */}
+      {/* <Introduction/> */}
+      {/* <Featured/> */}
+      {/* <lists/> */}
+    </div>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <section>
+        {/* logo */}
+        <div className="">
+            <img src="" alt="" />
+        </div>
+        {/* Menu mobile */}
+        {/* Menu Desktop */}
+    </section>
+  )
+}
+
+export default Navbar
