1cf3e01f29b83a81aaca015ed993c63913a11cb7
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2024-12-13 10:11:16 +0300

    Added the Navbar to the layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar"
 const App = () => {
   return (
     // This is for creating a padding on different device settings
-    <div className='px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64'>
+    <div className=''>
       <Navbar/>
       {/* <BreadCrumbs/> */}
       {/* <Introduction/> */}
