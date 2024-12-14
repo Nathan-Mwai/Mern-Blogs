a1d594ffe4db86de8a9f42b873436051d6e3f4f9
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2024-12-14 10:15:48 +0300

    Added initial header and styles to be used for the homepage

--- src/routes/Homepage.jsx
+++ src/routes/Homepage.jsx
@@ -10,6 +10,19 @@ const Homepage = () => {
         <span className="text-blue-800">Blogs and Articles</span>
       </div>
       {/* <Introduction/> */}
+      <div className="flex items-center justify-between">
+        {/* titles */}
+        <div className="">
+          <h1 className="text-2xl text-gray-800 md:text-5xl lg:text-6xl font-bold">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          </h1>
+          <p className="mt-8 text-md md:text-xl">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae ea tempora natus. Pariatur asperiores quam officia dolore.
+          </p>
+        </div>
+        {/* animated button */}
+        <Link to="write"></Link>
+      </div>
       {/* <Featured Posts/> */}
       {/* <Post Lists/> */}
     </div>
