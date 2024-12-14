3ea279e5d4f811f265ae65edece6d1680fd08d9e
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2024-12-14 10:08:50 +0300

    Added the initial homepage frontend view

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,9 @@ import Navbar from "./components/Navbar"
 const App = () => {
   return (
     // This is for creating a padding on different device settings
-    <div className=''>
+    <div className='px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64'>
       <Navbar/>
-      {/* <BreadCrumbs/> */}
-      {/* <Introduction/> */}
-      {/* <Featured/> */}
-      {/* <lists/> */}
+      
     </div>
   )
 }
--- src/routes/Homepage.jsx
+++ src/routes/Homepage.jsx
@@ -1,6 +1,18 @@
+import { Link } from "react-router-dom"
+
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <div className="mt-4 flex flex-col gap-4">
+      {/* <BreadCrumbs/> */}
+      <div className="flex gap-4">
+        <Link to="/">Home</Link>
+        <span>|</span>
+        <span className="text-blue-800">Blogs and Articles</span>
+      </div>
+      {/* <Introduction/> */}
+      {/* <Featured Posts/> */}
+      {/* <Post Lists/> */}
+    </div>
   )
 }
 
