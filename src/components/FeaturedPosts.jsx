7c451646cd914af618cfaf84ff770b940dc36e08
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2024-12-15 13:17:35 +0300

    Added the small details to the image and now the image is working well

--- src/components/FeaturedPosts.jsx
+++ src/components/FeaturedPosts.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import Image from './Image'
 const FeaturedPosts = () => {
   return (
@@ -6,14 +7,17 @@ const FeaturedPosts = () => {
         <div className="w-full lg:w-1/2 flex flex-col gap-4">
         {/* image */}
         <Image
-            path='featured1.jpeg'
+            src='featured1.jpeg'
             className='rounded-3xl object-cover'
         />
         {/* details */}
-        {/* <div className=''>
-            <h1>01.</h1>
-        </div> */}
+        <div className='flex items-center gap-4'>
+            <h1 className='font-semibold lg:text-lg'>01.</h1>
+            <Link className='text-blue-800 lg:text-lg'> Web Design</Link>
+            <span className='text-gray-500'>2 days ago</span>
+        </div>
         {/* title */}
+        <Link to='/test' className='text-xl lg:text-3xl font-semibold lg:front-bold'>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</Link>
         </div>
         {/* other posts */}
         <div className="w-full lg:w-1/2 flex flex-col gap-4">
