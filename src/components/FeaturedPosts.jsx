d16622c82d6569716fc7c19d190e285cc90dd654
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2024-12-15 13:36:03 +0300

    I'm now using grid which is better

--- src/components/FeaturedPosts.jsx
+++ src/components/FeaturedPosts.jsx
@@ -2,12 +2,12 @@ import { Link } from 'react-router-dom'
 import Image from './Image'
 const FeaturedPosts = () => {
   return (
-    <div className="mt-8 flex flex-col lg:flex-row gap-8">
-        {/* first */}
-        <div className="w-full lg:w-1/2 flex flex-col gap-4">
+    <div className="mt-8 grid grid-cols-1 lg:grid-cols-2 gap-8">
+    {/* first */}
+    <div className="flex flex-col gap-4">
         {/* image */}
         <Image
-            src='featured1.jpeg'
+            path='featured1.jpeg'
             className='rounded-3xl object-cover'
         />
         {/* details */}
@@ -17,14 +17,20 @@ const FeaturedPosts = () => {
             <span className='text-gray-500'>2 days ago</span>
         </div>
         {/* title */}
-        <Link to='/test' className='text-xl lg:text-3xl font-semibold lg:front-bold'>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</Link>
-        </div>
-        {/* other posts */}
-        <div className="w-full lg:w-1/2 flex flex-col gap-4">
-        {/* second */}
-        <div className='flex justify-between gap-4'></div>
-        </div>
+        <Link to='/test' className='text-xl lg:text-3xl font-semibold lg:front-bold'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        </Link>
     </div>
+    {/* other posts */}
+    <div className="flex flex-col gap-4">
+        {/* second */}
+        <div className='flex justify-between gap-4 bg-red-700'>rrrrrr</div>
+        {/* third */}
+        <div className='flex justify-between gap-4 bg-purple-800'>qqqqq</div>
+        {/* fourth */}
+        <div className='flex justify-between gap-4 bg-teal-600'>qsqsqs</div>
+    </div>
+</div>
   )
 }
 
