daac819ce51931f4c75a8c3f910bbebef9c05995
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2024-12-15 13:08:44 +0300

    Added outlook and in=mage of the featured post ... Hard coded for now but will be updating it from backend logic

--- src/components/FeaturedPosts.jsx
+++ src/components/FeaturedPosts.jsx
@@ -1,6 +1,25 @@
+import Image from './Image'
 const FeaturedPosts = () => {
   return (
-    <div>FeaturedPosts</div>
+    <div className="mt-8 flex flex-col lg:flex-row gap-8">
+        {/* first */}
+        <div className="w-full lg:w-1/2 flex flex-col gap-4">
+        {/* image */}
+        <Image
+            path='featured1.jpeg'
+            className='rounded-3xl object-cover'
+        />
+        {/* details */}
+        {/* <div className=''>
+            <h1>01.</h1>
+        </div> */}
+        {/* title */}
+        </div>
+        {/* other posts */}
+        <div className="w-full lg:w-1/2 flex flex-col gap-4">
+        
+        </div>
+    </div>
   )
 }
 
