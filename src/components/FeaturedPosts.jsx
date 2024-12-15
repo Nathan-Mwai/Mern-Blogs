e00e5f3fad94b091176d0b39168042af290c34f1
Nathan-Mwai <nathan.mwai@student.moringaschool.com>
2024-12-15 13:47:57 +0300

    finished on the second featured item and will adjust the item frame if need be i.e the image itself

--- src/components/FeaturedPosts.jsx
+++ src/components/FeaturedPosts.jsx
@@ -24,7 +24,23 @@ const FeaturedPosts = () => {
     {/* other posts */}
     <div className="flex flex-col gap-4">
         {/* second */}
-        <div className='flex justify-between gap-4 bg-red-700'>rrrrrr</div>
+        <div className='flex justify-between gap-4'>
+            <Image 
+            path='featured2.jpeg'
+            className="rounded-3xl object-cover w-1/3 aspect-video"
+            />
+            {/* details and title */}
+            <div className='w-2/3'>
+            {/* details */}
+            <div className=' flex items-center gap-4 text-sm lg:text-base mb-4'>
+                <h1 className='font-semibold'>02.</h1>
+                <Link className='text-blue-800'>Web Design</Link>
+                <span className='text-gray-500 text-sm '>2 days ago</span>
+            </div>
+            {/* title */}
+            <Link to='/test' className='text-base sm:text-lg md:text-2xl lg:text-xl xl:text-2xl font-medium'>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</Link>
+            </div>
+        </div>
         {/* third */}
         <div className='flex justify-between gap-4 bg-purple-800'>qqqqq</div>
         {/* fourth */}
